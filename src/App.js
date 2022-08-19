0e6cbca7fc1c39bc706855ec46063777f403fa1e
clogerie <66565287+clogerie@users.noreply.github.com>
2022-08-19 15:12:06 -0400

    LandingPage updates: added short bio and contact form, added styling, and added mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import LandingPage from "./Components/Pages/LandingPage";
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <LandingPage />
+    </>
   );
 }
 
