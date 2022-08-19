5b8004766f73680eb2e03223e64af792e755cd54
clogerie <66565287+clogerie@users.noreply.github.com>
2022-08-19 11:53:28 -0400

    added Navbar, added new name to README (using name PlugSpot for now)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     <h1>EV Charging Station Locator</h1>
-    </div>
+    <Navbar />
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <a href="/"><h2>PlugSpot</h2></a>
+            <a href="/">Home-About</a>
+            <a href="/">Signin</a>
+            <a href="/">Signup</a>
+            <a href="/">Dashboard</a>
+            <a href="/">Contact-Landing</a>
+        </nav>
+    );
+}
+
+export default Navbar;
