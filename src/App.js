23d4c345f086b8225714478a674fcb1393cac872
clogerie <66565287+clogerie@users.noreply.github.com>
2022-08-19 17:29:48 -0400

    added charging station and HERE Technologies image, added paragraphs explaining what PlugSite is and how it works, added mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import LandingPage from "./Components/Pages/LandingPage";
+import HomePage from './Components/Pages/HomePage';
 
 function App() {
   return (
     <>
       <Navbar />
       <LandingPage />
+      <HomePage />
       <Footer />
     </>
   );
--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -4,7 +4,24 @@ import "../Styles/HomePage.css";
 const HomePage = () => {
     return (
         <div className="home-container">
-          <h1>HomePage</h1>
+            <div className="about-plugspot">
+                <figure className="ev-charger-figure-1">
+                  <img src="https://images.unsplash.com/photo-1616361715059-9daf3fdd50be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80" alt="ev chargin station" />
+                </figure>
+                <article className="about-article">
+                  <h2>What is PlugSpot?</h2>
+                  <p>PlugSpot is a web application built to help the community of electric car owners. Get started by continuing as a guest or create an account to find the a charging station near you!</p>
+                </article>
+            </div>
+            <div className="about-here-api">
+                <article className="about-article">
+                  <h2>How does it work?</h2>
+                  <p>PlugSpot uses the EV Charging Station API powered by HERE Technologies. This is a RESTful API that provides lists of charging station locations. It also includes information on connector types, hours of operation, and dynamic data about connector availability at charging stations for electric vehicles.</p>
+                </article>
+                <figure className="here-api-figure">
+                  <img src="https://developer.here.com/images/herelogo.png" alt="here logo" />
+                </figure>
+            </div>
         </div>
     );
 }
