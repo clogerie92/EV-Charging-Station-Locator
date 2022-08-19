6c1691a1781bb05a7f7b3f8d1a28934ce00c89f5
clogerie <66565287+clogerie@users.noreply.github.com>
2022-08-19 12:28:57 -0400

    navbar styling: changed navbar color, navbar links color, removed underline and bulletpoints, added mobile responsiveness

---
+++ src/Components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Styles/components.css";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="brand">
+                <a href="/">PlugSpot</a>
+            </div>
+            <div className="navbar-links">
+                <ul>
+                    <li><a href="/">Home-About</a></li>
+                    <li><a href="/">Signin</a></li>
+                    <li><a href="/">Signup</a></li>
+                    <li><a href="/">Dashboard</a></li>
+                    <li><a href="/">Contact-Landing</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
