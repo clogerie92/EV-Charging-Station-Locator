6b9d0eadd2e3d931c5b5c6d3282bfa6022d8175e
clogerie <66565287+clogerie@users.noreply.github.com>
2022-08-20 11:56:51 -0400

    added transparent background color to signup form, added button to signin if user already has an account

--- src/Components/Pages/Signup.js
+++ src/Components/Pages/Signup.js
@@ -4,12 +4,16 @@ import "../Styles/Signup.css";
 const Signup = () => {
     return (
         <div className="signup-container">
-            <h2>Signup</h2>
             <div className="signup-form-container">
+                <h2>Signup</h2>
                 <input placeholder="Username" type="text" />
                 <input placeholder="Email" type="text" />
                 <input placeholder="Password" type="text" />
                 <button type="submit">Signup</button>
+                <div className="signin-portal">
+                    <p>Already have an account? Signin here...</p>
+                    <button type="submit">Signin</button>
+                </div>
             </div>
         </div>
     );
