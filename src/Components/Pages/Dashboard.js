0d73e9467962f9b4a729380e606814908cf35606
clogerie <66565287+clogerie@users.noreply.github.com>
2022-08-21 12:19:26 -0400

    fixed styling bug in dashboard: search container div now expands with child elements

--- src/Components/Pages/Dashboard.js
+++ src/Components/Pages/Dashboard.js
@@ -3,22 +3,30 @@ import "../Styles/Dashboard.css";
 
 const Dashboard = () => {
     return (
-        <div className="dashboard-container">
-            <div className="search-container">
-                <input placeholder="Enter search here..." type="text" />
-                <div className="btn-container">
-                    <button>Search</button>
-                    <button>Empty</button>
-                </div>
-                <div className="history-container">
-                    {/* dynamically render history cards */}
-                </div>
-            </div>    
-            <div className="location-container">
-                {/* dynamically render chargin station location cards */}
-                <h2>Locations</h2>
+        <>
+            <header className="fluid"><h1>Dashboard</h1></header>
+            <div className="dashboard-container">
+                <aside>
+                    <div className="search-container">
+                        <input placeholder="Enter search here..." type="text" />
+                        <div className="btn-container">
+                            <button>Search</button>
+                            <button>Clear</button>
+                        </div>
+                        <div className="history-container">
+                            {/* dynamically render history cards */}
+                        </div>
+                    </div>     
+                </aside>   
+                <main>
+                    <div className="location-container">
+                        {/* dynamically render chargin station location cards */}
+                        <h2>Locations</h2>
+                        {/* <input placeholder="test" type="text" /> */}
+                    </div>
+                </main>
             </div>
-        </div>
+        </>
     );
 }
 
